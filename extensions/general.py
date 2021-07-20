19d9e7013c62814b107c9ecbe5761deb3c924a47
NP <76001641+CoderN-P@users.noreply.github.com>
2021-07-20 17:05:47 +0000

    fixed spellling issue

--- extensions/general.py
+++ extensions/general.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import country_converter as coco
 import random
 from fuzzywuzzy import fuzz
-from monogmethods import get_prefix
+from mongomethods import get_prefix
 from countryinfo import CountryInfo
 
 global quiz_country_list
