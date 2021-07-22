091df7399d1e197376fc858db31ac3ef7d06585c
NP <76001641+CoderN-P@users.noreply.github.com>
2021-07-22 04:01:34 +0000

    fixed some more bugs

--- main.py
+++ main.py
@@ -621,9 +621,9 @@ async def on_ready():
 @bot.listen()
 async def on_message(msg):
 
-  if bot.user.mentioned_in(message) and message.mention_everyone is False and 'prefix' in message:
+  if bot.user.mentioned_in(msg) and msg.mention_everyone is False and 'prefix' in msg:
         prefix = await get_prefix(msg.guild.id)
-        await message.channel.send(f"My prefix in this server is {prefix}")
+        await msg.channel.send(f"My prefix in this server is {prefix}")
 
   
 
