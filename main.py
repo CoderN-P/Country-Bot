7bc2c2bd4b0f7553e7f169cd4f30a3771b50256c
NP <76001641+CoderN-P@users.noreply.github.com>
2021-07-22 17:10:21 +0000

    fixed more prefix stuff

--- main.py
+++ main.py
@@ -621,10 +621,7 @@ async def on_ready():
 @bot.listen()
 async def on_message(msg):
 
-  if bot.user.mention in msg.content.split()[0] and msg.mention_everyone is False:
-        prefix = await get_prefix(msg.guild.id)
-        await msg.channel.send(f"My prefix in this server is `{prefix}`")
-  elif bot.user.mention in msg.content.split()[0] and msg.mention_everyone is False and 'prefix' in msg.content:
+  if msg.content == f'{bot.user.mention} prefix' or msg.content == f'{bot.user.mention}prefix':
         prefix = await get_prefix(msg.guild.id)
         await msg.channel.send(f"My prefix in this server is `{prefix}`")
 
