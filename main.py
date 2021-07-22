cc57b0992b442d412ad4c62c19f4193ad330200f
NP <76001641+CoderN-P@users.noreply.github.com>
2021-07-22 16:58:24 +0000

    fixed sum more bugs

--- main.py
+++ main.py
@@ -624,7 +624,7 @@ async def on_message(msg):
   if bot.user.mention in msg.content.split()[0] and msg.mention_everyone is False:
         prefix = await get_prefix(msg.guild.id)
         await msg.channel.send(f"My prefix in this server is `{prefix}`")
-  elif bot.user.mention in msg.content.split()[0] and msg.mention_everyone is False and 'prefix' in msg.content.split()[1]:
+  elif bot.user.mention in msg.content.split()[0] and msg.mention_everyone is False and 'prefix' in msg.content:
         prefix = await get_prefix(msg.guild.id)
         await msg.channel.send(f"My prefix in this server is `{prefix}`")
   
