2113f2707bf665484534a0bc059fc0350652e698
NP <76001641+CoderN-P@users.noreply.github.com>
2021-07-31 16:35:24 +0000

    fixed syntax

--- main.py
+++ main.py
@@ -379,7 +379,7 @@ async def on_command_error(ctx, error):
       await ctx.channel.send(f"Did you mean {similar}")
 
     elif isinstance(error, discord.ext.commands.errors.CommandOnCooldown):
-       pawait ctx.send(embed=discord.Embed(title='Incorrect Usage', description=f"Correct Usage: ```{ctx.prefix}{ctx.command.name} {ctx.command.signature}```", color=discord.Colour.red()))
+       await ctx.send(embed=discord.Embed(title='Incorrect Usage', description=f"Correct Usage: ```{ctx.prefix}{ctx.command.name} {ctx.command.signature}```", color=discord.Colour.red()))
 
     
 
